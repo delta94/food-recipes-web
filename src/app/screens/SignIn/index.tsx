58d449313adadb941ed21eace28dfa41d18731a6
unknown <viniciusbeloti7@gmail.com>
2020-09-27 21:40:25 -0300

    chore: add react-toastify

--- src/app/screens/SignIn/index.tsx
+++ src/app/screens/SignIn/index.tsx
@@ -1,18 +1,38 @@
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import logo from '../../../assets/img/logo.png'
 import { Creators } from '../../../core/redux/store/ducks/auth'
+import * as Yup from 'yup'
 
 import { Container, Content, AnimationContainer, Background } from './styles'
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O email é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória')
+})
+
 const SignIn = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const dispatch = useDispatch()
 
-  function handleSubmit (event: ChangeEvent<HTMLFormElement>) {
+  async function handleSubmit (event: ChangeEvent<HTMLFormElement>) {
     event.preventDefault()
-    dispatch(Creators.login('pass', 'asasa'))
+
+    try {
+      const validate = await schema.validate(schema)
+      console.log(validate)
+    } catch (error) {
+      console.log(error)
+    }
+
+    // dispatch(Creators.login('pass', 'asasa'))
   }
+
   return (
     <Container>
       <Content>
@@ -22,12 +42,13 @@ const SignIn = () => {
           <form onSubmit={handleSubmit}>
             <h1>Faça seu login</h1>
 
-            <input name="email" placeholder="E-mail" />
+            <input name="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
 
             <input
               name="password"
               type="password"
               placeholder="Senha"
+              value={password} onChange={(e) => setPassword(e.target.value)}
             />
 
             <button type="submit">Entrar</button>
