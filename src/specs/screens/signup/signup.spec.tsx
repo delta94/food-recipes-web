143d6c1479a8079285fb374c555d4bf82bebd30d
unknown <viniciusbeloti7@gmail.com>
2020-09-30 19:05:09 -0300

    chore: eslint config

--- src/specs/screens/signup/signup.spec.tsx
+++ src/specs/screens/signup/signup.spec.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { act, fireEvent, render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import * as redux from 'react-redux'
 
 import SignUp from '../../../app/screens/SignUp'
 
