f6e2fe6dd865487f0e80ef6438fc70d4301acc1c
unknown <viniciusbeloti7@gmail.com>
2020-09-30 17:15:06 -0300

    chore: update url config

--- src/config/url.ts
+++ src/config/url.ts
@@ -1,4 +1,8 @@
 // URL e PORTA base vinda da aplicação API
 
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 const baseURL = process.env.BASE_URL || 'http://localhost:3333'
 export default baseURL
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app/screens/App'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 ReactDOM.render(
   <React.StrictMode>
