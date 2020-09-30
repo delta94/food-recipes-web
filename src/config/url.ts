36b222b451c3ecfd0851e6829bacb1afc7cbd54e
unknown <viniciusbeloti7@gmail.com>
2020-09-30 17:53:39 -0300

    chore: update dotenv

--- src/config/url.ts
+++ src/config/url.ts
@@ -1,8 +1,4 @@
 // URL e PORTA base vinda da aplicação API
 
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-const baseURL = 'http://localhost:3333' || 'https://food-recipes-api-1.herokuapp.com/'
+const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:3333'
 export default baseURL
